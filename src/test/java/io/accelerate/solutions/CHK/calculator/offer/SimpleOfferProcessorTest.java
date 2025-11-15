7ad2630718f3cfd1172f62b50ca89cdfa9e2d4c0
sbx_user1051 <sbx_user1051@169.254.2.237>
2025-11-15 15:22:24 +0000

    Sat Nov 15 15:22:24 UTC 2025

--- src/test/java/io/accelerate/solutions/CHK/calculator/offer/SimpleOfferProcessorTest.java
+++ src/test/java/io/accelerate/solutions/CHK/calculator/offer/SimpleOfferProcessorTest.java
@@ -1,5 +1,6 @@
 package io.accelerate.solutions.CHK.calculator.offer;
 
+import io.accelerate.solutions.CHK.model.Basket;
 import io.accelerate.solutions.CHK.model.ItemType;
 import io.accelerate.solutions.CHK.model.offer.SimpleOffer;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,14 +21,10 @@ public class SimpleOfferProcessorTest {
 
     @Test
     public void shouldProcessItemsInBasket() {
-        Map<ItemType, Long> itemsInBasket = Map.of(
-                ItemType.A, 7L,
-                ItemType.B, 2L,
-                ItemType.C, 5L
-        );
+        Basket basket = Basket.fromSkus("AAAAAAABBCCCCC");
         SimpleOffer simpleOffer = SimpleOffer.of(ItemType.A, 3, 130);
 
-        SpecialOfferResult result = processor.process(simpleOffer, itemsInBasket);
+        SpecialOfferResult result = processor.process(simpleOffer, basket);
 
         assertEquals(result.getItemsProcessed(), Map.of(ItemType.A, 6L));
         assertEquals(result.getTotalPriceApplied(), 260);

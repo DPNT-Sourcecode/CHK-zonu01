d581a5bd7366cc1413e58e46a13af4418f6d1c30
sbx_user1051 <sbx_user1051@169.254.2.237>
2025-11-15 13:16:11 +0000

    Sat Nov 15 13:16:11 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/calculator/CheckoutCalculator.java
+++ src/main/java/io/accelerate/solutions/CHK/calculator/CheckoutCalculator.java
@@ -1,13 +1,20 @@
 package io.accelerate.solutions.CHK.calculator;
 
 import io.accelerate.solutions.CHK.model.Basket;
+import io.accelerate.solutions.CHK.model.ItemType;
 import io.accelerate.solutions.CHK.model.SpecialOffer;
+
+import java.util.List;
 
 public class CheckoutCalculator {
 
-    List<SpecialOffer>
+    List<SpecialOffer> specialOffers = List.of(
+        SpecialOffer.builder().itemType(ItemType.A).targetAmount(3).totalBundlePrice(130).build(),
+        SpecialOffer.builder().itemType(ItemType.B).targetAmount(2).totalBundlePrice(130).build()
+    );
 
     public Integer calculateTotalPrice(Basket basket) {
         return 0;
     }
 }
+
--- src/main/java/io/accelerate/solutions/CHK/model/SpecialOffer.java
+++ src/main/java/io/accelerate/solutions/CHK/model/SpecialOffer.java
@@ -1,8 +1,21 @@
 package io.accelerate.solutions.CHK.model;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class SpecialOffer {
 
     private ItemType itemType;
     private int targetAmount;
     private int totalBundlePrice;
+
+    @Builder.
+    private final int appliedDiscount;
+
+    private SpecialOffer(ItemType itemType, int targetAmount, int totalBundlePrice) {
+
+    }
 }
+

a394c57b14df837a04060b528eabe026a278b0d7
sbx_user1051 <sbx_user1051@169.254.80.97>
2025-11-15 12:13:11 +0000

    Sat Nov 15 12:13:11 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -3,10 +3,15 @@ package io.accelerate.solutions.CHK;
 import io.accelerate.solutions.CHK.model.ItemType;
 
 import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         Map<ItemType, Integer> basket = skus.chars()
-                .
+                .collect(groupingBy(Function.identity(), counting()));
     }
 }
+

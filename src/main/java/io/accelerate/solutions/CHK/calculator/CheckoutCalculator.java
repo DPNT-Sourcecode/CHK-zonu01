73c814536ee26c453771049995a6da867cb10812
sbx_user1051 <sbx_user1051@169.254.2.237>
2025-11-15 13:10:11 +0000

    Sat Nov 15 13:10:11 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/calculator/CheckoutCalculator.java
+++ src/main/java/io/accelerate/solutions/CHK/calculator/CheckoutCalculator.java
@@ -1,8 +1,11 @@
 package io.accelerate.solutions.CHK.calculator;
 
 import io.accelerate.solutions.CHK.model.Basket;
+import io.accelerate.solutions.CHK.model.SpecialOffer;
 
 public class CheckoutCalculator {
+
+    List<SpecialOffer>
 
     public Integer calculateTotalPrice(Basket basket) {
         return 0;
---
+++ src/main/java/io/accelerate/solutions/CHK/model/SpecialOffer.java
@@ -0,0 +1,8 @@
+package io.accelerate.solutions.CHK.model;
+
+public class SpecialOffer {
+
+    private ItemType itemType;
+    private int targetAmount;
+    private int totalBundlePrice;
+}
